c20e362aa9fe0fc5db80ad6d5a74f0c1cf726098
kiran-rhv <r.kiran@dollarbirdinc.comm>
2022-01-27 10:51:41 +0530

    Bootstrap validation and JS updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,46 +1,92 @@
+/* ------------------ Form Validation Start ------------------ */
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            } else {
+              event.preventDefault();
+              checkMonth();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
+/* ------------------ Form Validation End ------------------ */
+
 function checkMonth(){
-  let month = document.getElementById('monthInput').value;
-  switch (month) {
-    // case with 31 Days 
-    case "1":
-    case "3":
-    case "5":
-    case "7":
-    case "8":
-    case "10":
-    case "12":
-    document.getElementById("demo1").innerHTML = "31 Days in a Month";
-    break;
+  let output = "";
+  let isMonthNotANumber = isNaN(document.getElementById("monthInput").value);
+  let isYearNotANumber = isNaN(document.getElementById("yearInput").value);
 
-    // case with 30 Days 
-    case "4":
-    case "6":
-    case "9":
-    case "11":
-    document.getElementById("demo2").innerHTML = "30 Days in a Month";
-    break;
+  if (isMonthNotANumber || isYearNotANumber) {
+    output = "Invalid Month or Year";
+    document.getElementById("output").innerHTML = output;
+    return;
+  } else {
+    let month = parseInt(document.getElementById("monthInput").value);
+    let checkyear = document.getElementById("yearInput").value;
 
-    // case with leap year
-    case "2":
-    leapyear();
-    break;
+    switch (month) {
+      // case with 31 Days 
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        output = "31 Days in a Month";
+        document.getElementById("output").innerHTML = output;
+        break;
 
-    default:
-    document.getElementById("demo3").innerHTML = "Invalid Month";
-    break;
+      // case with 30 Days 
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        output = "30 Days in a Month";
+        document.getElementById("output").innerHTML = output;
+      break;
+
+      // case with leap year
+      case 2:
+      leapyear(checkyear);
+      break;
+
+      default:
+        output = "Invalid Month";
+        document.getElementById("output").innerHTML = output;
+      break;
+    }
   }
 }
 
-function leapyear() {
-  let checkyear = document.getElementById("yearInput").value;
-  let feb;
+function leapyear(checkyear) {
+  let feb = "";
 
   if ((checkyear % 4 == 0) && (checkyear % 100 !== 0) || (checkyear % 400 == 0)) {
-    feb = checkyear + " is a Leap year" + " and its 29 days in a Month";
+    feb = checkyear + " is a Leap year and its 29 days in a Month";
   } else {
-    feb = checkyear + " is not a Leap year" + " and its 28 days in a Month";
+    feb = checkyear + " is not a Leap year and its 28 days in a Month";
   }
-  document.getElementById('leap').innerHTML = feb;
+  document.getElementById('output').innerHTML = feb;
 }
 
 
